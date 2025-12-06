5c6df76808e456a546e3d123b1f01c174d3f6089
Yesenia Guzman <YXG240011@utdallas.edu>
2025-12-06 17:53:07 -0600

    debugged

--- src/ArtQuiz.py
+++ src/ArtQuiz.py
@@ -6,8 +6,16 @@ def main():
     pygame.display.set_caption("Art Quiz")
     resolution = (800, 600)
     screen = pygame.display.set_mode(resolution)
+
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+        screen.fill('Green')
+        pygame.display.flip()
     pygame.quit()
+    
 
-
-    if __name__ == "__main__":
-        main()
+if __name__ == "__main__":
+    main()
