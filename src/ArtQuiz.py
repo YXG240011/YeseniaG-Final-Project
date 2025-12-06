4ef63995883e357733994c7dbc1e56bcdf748986
Yesenia Guzman <YXG240011@utdallas.edu>
2025-12-05 22:16:39 -0600

    created screen

--- src/ArtQuiz.py
+++ src/ArtQuiz.py
@@ -2,3 +2,12 @@ import pygame
 
 
 def main():
+    pygame.init()
+    pygame.display.set_caption("Art Quiz")
+    resolution = (800, 600)
+    screen = pygame.display.set_mode(resolution)
+    pygame.quit()
+
+
+    if __name__ == "__main__":
+        main()
