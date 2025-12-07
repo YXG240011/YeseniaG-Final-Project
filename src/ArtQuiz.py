0158ccb95239a0e56c36a6a4a23c79d6e5ee9482
Yesenia Guzman <YXG240011@utdallas.edu>
2025-12-06 19:15:24 -0600

    added button

--- src/ArtQuiz.py
+++ src/ArtQuiz.py
@@ -1,4 +1,5 @@
 import pygame
+import random
 
 
 class Particle():
@@ -8,13 +9,43 @@ class Particle():
         self.size = size
         self.color = 'Green'
         
+class Button():
+    def __init__(self, x, y, width, height, color, color2, text, action):
+        self.rect = pygame.Rect(x, y, width, height)
+        self.text = text
+        self.action = action
+        self.color = color
+        self.hover_color = color2
+        self.current_color = color
+        self.font = pygame.font.Font(None, 30)
+    
+    def draw(self, screen):
+        pygame.draw.rect(screen,self.current_color,self.rect)
+        text_surface = self.font.render(self.text, True, (0, 0, 0))
+        text_rect = text_surface.get_rect(center=self.rect.center)
+        screen.blit(text_surface, text_rect)
 
+    def handle_event(self, event):
+        if event.type == pygame.MOUSEMOTION:
+            if self.rect.collidepoint(event.pos):
+                self.current_color = self.hover_color
+            else:
+                self.current_color = self.color
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            if self.rect.collidepoint(event.pos):
+                self.action()
+
+def button_function():
+    #function stuffs
+
+    return 0
 
 def main():
     pygame.init()
     pygame.display.set_caption("Art Quiz")
     resolution = (800, 600)
     screen = pygame.display.set_mode(resolution)
+    button = Button(100,100,100,50, (255,0,200), (200,0,255), "67", button_function)
     particle = Particle()
     print(particle.pos)
     print(particle.size)
@@ -25,7 +56,11 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
+            button.handle_event(event)
         screen.fill('lavender')
+        button.draw(screen)
+
+
         pygame.display.flip()
     pygame.quit()
     
