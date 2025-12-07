521f5fac85b631a56494bff980ec69040dcbf058
Yesenia Guzman <YXG240011@utdallas.edu>
2025-12-06 18:20:54 -0600

    made background lavender

--- src/ArtQuiz.py
+++ src/ArtQuiz.py
@@ -1,18 +1,31 @@
 import pygame
 
 
+class Particle():
+
+    def __init__(self, pos=(0, 0), size=15):
+        self.pos = pos
+        self.size = size
+        self.color = 'Green'
+        
+
+
 def main():
     pygame.init()
     pygame.display.set_caption("Art Quiz")
     resolution = (800, 600)
     screen = pygame.display.set_mode(resolution)
+    particle = Particle()
+    print(particle.pos)
+    print(particle.size)
+    print(particle.color)
 
     running = True
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
-        screen.fill('Green')
+        screen.fill('lavender')
         pygame.display.flip()
     pygame.quit()
     
